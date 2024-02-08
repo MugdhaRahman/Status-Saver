e9b522fd133de23170ee109f0a36dfaf0a360547
Mugdha <mugdharahman11@gmail.com>
2024-02-08 19:47:59 +0600

    dark mode settings

--- app/src/main/java/com/androvine/statussaver/MainApplication.kt
+++ app/src/main/java/com/androvine/statussaver/MainApplication.kt
@@ -1,7 +1,9 @@
 package com.androvine.statussaver
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelStore
+import androidx.preference.PreferenceManager
 import com.androvine.statussaver.permissionMVVM.permissionModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -22,24 +24,24 @@ class MainApplication : Application() {
             modules(permissionModule)
         }
 
-//        // set app dark or light mode based on user preference
-//        val themeMode = PreferenceManager.getDefaultSharedPreferences(this)
-//            .getString("pref_dark_theme", "system_default")
-//
-//
-//        when (themeMode) {
-//            "light_theme" -> {
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//            }
-//
-//            "dark_theme" -> {
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-//            }
-//
-//            else -> {
-//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-//            }
-//        }
-//
+        // set app dark or light mode based on user preference
+        val themeMode = PreferenceManager.getDefaultSharedPreferences(this)
+            .getString("pref_dark_theme", "system_default")
+
+
+        when (themeMode) {
+            "light_theme" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            "dark_theme" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+
+            else -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
+
     }
 }
