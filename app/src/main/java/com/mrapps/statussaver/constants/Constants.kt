52ab34781b4dd49272189f7d559619b8f2400670
Mugdha <mugdharahman11@gmail.com>
2024-04-28 15:43:46 +0600

    fix

--- app/src/main/java/com/mrapps/statussaver/constants/Constants.kt
+++ app/src/main/java/com/mrapps/statussaver/constants/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
         val contactEmail = "contact@email.com"
         val privacyPolicyUrl = "https://www.google.com"
         val appVersion = "1.0"
-        val companyName = "Androvine"
+        val companyName = "MrApps"
 
     }
 
